ac4334798dfc9d032d8ee523fda63c094e21d27e
Juan Dominguez <juanignaciodom3@gmail.com>
2023-05-20 00:29:18 -0300

    add head to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Digyvira | Construye tu arbol familiar</title>
+    <meta name="description" content="Sitio web para armar tu arbol genealogico">
+    <meta name="keywords" content="transgeneracional, arbol, digyvira, ancestros, familia, sistema">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
+
+<h1>Bienvenido a Digyvira</h1>
